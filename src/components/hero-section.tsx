1c20938f3e44210a64fbe9d29120420eb11720e3
Opaius <vgromania3@gmail.com>
2025-11-28 13:19:10 +0200

    Remove particle hover
    Signed-off-by: Opaius <vgromania3@gmail.com>

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -19,7 +19,7 @@ export function HeroSection() {
           particleSpread={10}
           speed={0.1}
           particleBaseSize={200}
-          moveParticlesOnHover={true}
+          moveParticlesOnHover={false}
           alphaParticles={false}
           disableRotation={false}
         />
